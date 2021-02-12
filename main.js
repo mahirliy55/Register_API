0a79154cf441c853680278e9e3652bbc97514197
mahirliy55 <mahirliy55@gmail.com>
2021-02-12 17:31:47 -0500

    Completed Register API

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 let url = "http://127.0.0.1:8000/api/v1/show-users/"
-let postUrl = "http://127.0.0.1:8000/api/v1/register/"
 let card = document.getElementById("card")
 let btn = document.getElementById("btn")
 
@@ -12,19 +11,19 @@ sendRequest = () => {
             <div class="card" style="width: 18rem;">
             <img src="${data[i].image}" class="card-img-top" alt="...">
             <div class="card-body">
-              <h5 class="card-title">${data[i].username}</h5>
+            <h5 class="card-title">${data[i].username}</h5>
               <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-            <ul class="list-group list-group-flush">
+              </div>
+              <ul class="list-group list-group-flush">
               <li class="list-group-item">First name: <strong>${data[i].first_name}</strong></li>
               <li class="list-group-item">Last name: <strong>${data[i].last_name}</strong></li>
               <li class="list-group-item">Email: <strong>${data[i].email}</strong></li>
-            </ul>
+              </ul>
             <div class="card-body">
-              <a href="#" class="card-link">Card link</a>
+            <a href="#" class="card-link">Card link</a>
               <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
+              </div>
+              </div>
             `
         }
 
@@ -35,40 +34,40 @@ sendRequest()
 // -------------------------------------------------------------------------------------
 
 
-let user = document.getElementById("user").value
-let fname = document.getElementById("fname").value
-let lname = document.getElementById("lname").value
-let email = document.getElementById("email").value
-let file = document.getElementById("file").value
-let pass = document.getElementById("pass").value
-let pass2 = document.getElementById("pass2").value
 let sub = document.getElementById("sub")
 
 
-posted = (username, first_name, last_name, email, image, password, password2) => {
-    sub.addEventListener("submit", () => {
-        let iii = {
-            "username": username,
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
-            "image": image,
-            "password": password,
-            "password2": password2,
-        }
-        fetch(postUrl, {
-            method: "POST",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify(iii)
-        }).then(resp => resp.json()).then(data => {
-            console.log(data);
-        })
+let postUrl = "http://127.0.0.1:8000/api/v1/register/"
+posted = (username, first_name, last_name, email, password, password2) => {
+    let iii = {
+        "username": username,
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "password": password,
+        "password2": password2,
+    }
+    fetch(postUrl, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify(iii)
+    }).then(resp => resp.json()).then(data => {
+        console.log(data);
     })
+
 }
 
-sub.addEventListener("submit", () => {
+sub.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let user = document.getElementById("user").value
+    let fname = document.getElementById("fname").value
+    let lname = document.getElementById("lname").value
+    let email = document.getElementById("email").value
+    let pass = document.getElementById("pass").value
+    let pass2 = document.getElementById("pass2").value
+
+    posted(user, fname, lname, email, pass, pass2)
 
 })
-posted(user, fname, lname, email, file, pass, pass2)
